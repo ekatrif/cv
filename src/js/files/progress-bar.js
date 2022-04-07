59edf7bc1f5a05f3a3104ba1a5b32b0b867805e4
ekatrif <katiapervak@gmail.com>
2022-04-07 15:15:39 +0300

    2scripts added - qualities & progress-bar

---
+++ src/js/files/progress-bar.js
@@ -0,0 +1,34 @@
+let currentWidth;
+let speed = 0;
+let maxWidth;
+let progress = document.getElementsByClassName("skill__bar");
+for (const element of progress) {
+  element.style.display = "none";
+}
+
+async function loadBars(array) {
+  for (const item of array) {
+    await drawBar(item);
+  }
+}
+
+loadBars(progress);
+
+async function drawBar(currentBar) {
+  currentBar.style.display = "block";
+  currentWidth = 0;
+  maxWidth = parseInt(currentBar.style.width);
+  let interval = setInterval(function () {
+    currentBar.style.width = currentWidth + "%";
+    let sign = Math.random() > 0.5 ? 1 : -1;
+    if (speed > 0) speed += sign * Math.random() * 0.4;
+    if (speed <= 0) speed = 0.4;
+    if (currentWidth >= maxWidth) {
+      speed = 0;
+    } else {
+      currentWidth += speed;
+    }
+  }, 10);
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  clearInterval(interval);
+}
---
+++ src/js/files/qualities.js
@@ -0,0 +1,14 @@
+let qualities = document.getElementsByClassName("quality__bar");
+let emptyBox = `<li class="quality__item">&nbsp;</li>`;
+let fullBox = `<li class="quality__item quality__item_full">&nbsp;</li>`;
+for (let item of qualities) {
+  let currentProgress = item.getAttribute("progress");
+  console.log(currentProgress);
+  for (let i = 1; i <= 5; i++) {
+    if (i <= currentProgress) {
+      item.insertAdjacentHTML("afterbegin", fullBox);
+    } else {
+      item.insertAdjacentHTML("beforeend", emptyBox);
+    }
+  }
+}
