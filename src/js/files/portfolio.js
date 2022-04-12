d8273174d8253219048397fb3584795ad2230827
ekatrif <katiapervak@gmail.com>
2022-04-12 17:06:02 +0300

    Filter for Portfolio added

--- src/js/files/portfolio.js
+++ src/js/files/portfolio.js
@@ -73,16 +73,74 @@ const portfolioItems = [
 ];
 
 const portfolioContainer = document.getElementById("portfolio-container");
-let itemTemplate;
-for (const portfolioItem of portfolioItems) {
-  itemTemplate = `<div class="portfolio__screenshot project">
-<img src="${portfolioItem.img}" alt="" />
-<div class="project__about about-project">
-<h3 class="about-project__title">${portfolioItem.title}</h3>
-<div class="about-project__url">
-<span>URL: </span><a href="${portfolioItem.url}" target="_blank" rel="nofollow">${portfolioItem.url}</a>
-</div>
-</div>
-</div>`;
-  portfolioContainer.insertAdjacentHTML("beforeend", itemTemplate);
+const portfolioTags = document.getElementById("portfolio-tags");
+
+//Отображает элементы портфолио
+
+function insertPortfolioItems(array) {
+  let itemTemplate;
+  for (const portfolioItem of array) {
+    itemTemplate = `<div class="portfolio__screenshot project">
+    <img src="${portfolioItem.img}" alt="" />
+    <div class="project__about about-project">
+    <h3 class="about-project__title">${portfolioItem.title}</h3>
+    <div class="about-project__url">
+    <span>URL: </span><a href="${portfolioItem.url}" target="_blank" rel="nofollow">${portfolioItem.url}</a>
+    </div>
+    </div>
+    </div>`;
+    portfolioContainer.insertAdjacentHTML("beforeend", itemTemplate);
+  }
+}
+insertPortfolioItems(portfolioItems);
+
+//Устанавливаем слушатель событий на плашки-теги через делегирование
+portfolioTags.addEventListener("click", function (e) {
+  hideContent();
+  toggleTagClass(e);
+  const currentTag = e.target.getAttribute("data-tag");
+  getFiteredItems(currentTag);
+  showContent();
+});
+
+//Переключение стилей плашек тегов
+function toggleTagClass(e) {
+  const tagsCollection = e.currentTarget.childNodes;
+  for (let item of tagsCollection) {
+    if (
+      item.tagName &&
+      item.tagName.toLowerCase() === "li" &&
+      item.classList.contains("tag-active")
+    ) {
+      item.classList.remove("tag-active");
+      item.classList.add("tag-noactive");
+    }
+  }
+  e.target.classList.remove("tag-noactive");
+  e.target.classList.add("tag-active");
+}
+
+//Анимация при исчезновении
+function hideContent() {
+  if (portfolioContainer.classList.contains("show")) {
+    portfolioContainer.classList.remove("show");
+  }
+  portfolioContainer.classList.add("hide");
+}
+//Анимация при появлении
+function showContent() {
+  setTimeout(function () {
+    if (portfolioContainer.classList.contains("hide")) {
+      portfolioContainer.classList.remove("hide");
+    }
+    portfolioContainer.classList.add("show");
+  }, 500);
+}
+//Фильтруем элементы портфолио по тегам
+async function getFiteredItems(tag) {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  let filteredArray = [];
+  filteredArray = portfolioItems.filter((item) => item.tags.includes(tag));
+  portfolioContainer.textContent = "";
+  insertPortfolioItems(filteredArray);
 }
