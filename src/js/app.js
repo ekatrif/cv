46d4df6ab9b349174b18a8025bd564ac8bf6b340
ekatrif <katiapervak@gmail.com>
2023-10-10 15:38:28 +0300

    feat: add deploy in russian to separate branch

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-// Включить/выключить FLS (Full Logging System) (в работе)
-window["FLS"] = true;
-
-// Подключение основного файла стилей
-import "../scss/style.scss";
-
-/* Подключаем рендер html элементов */
-import renderHtml from '../index.js';
-renderHtml();
